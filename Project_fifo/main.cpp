1305ecf64fdb938db81b4187b9b3d8bad81392c8
Lucas-Rodrigues2 <lucas.rodrigues@eut.univ-amu.fr>
2023-10-16 10:29:38 +0200

    test semaphore with threads

--- Project_fifo/main.cpp
+++ Project_fifo/main.cpp
@@ -1,9 +1,44 @@
 #include <iostream>
+#include <vector>
+#include <mutex>
+#include <semaphore>
+#include <thread>
 
 using namespace std;
 
+counting_semaphore semaphore{5};
+
+void fct(unsigned j){
+    cout << "test n°" << j << endl;
+    cout << "test id : " << this_thread::get_id() << endl;
+
+    this_thread::sleep_for(100000ms); // do its action
+
+    cout << "finish n°" << j << endl;
+    semaphore.release();
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    unsigned j = 0;
+    vector<thread> threads;
+
+    while(j < 100) {
+        ++j;
+
+        if(semaphore.try_acquire()){
+            threads.push_back(thread(fct, j));
+        }
+        else{
+            cout << "block n°" << j << endl;
+        }
+
+        this_thread::sleep_for(5000ms);
+    }
+
+    for (thread & customer : threads) {
+        customer.join();
+    }
+
     return 0;
 }
